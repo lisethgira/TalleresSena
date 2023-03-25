d39f6e695ec7ae777866bdfa563748658787f912
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2023-03-25 14:14:18 -0500

    feat: se crea aarchivo de css grid

--- Taller_Basico_JavaScript/scripts/ejercicios.js
+++ Taller_Basico_JavaScript/scripts/ejercicios.js
@@ -2,7 +2,7 @@
 function convertToHours() {
     const minutes = parseInt(document.getElementById("minutes").value);
     const hours = minutes / 60;
-    document.getElementById("result").textContent = `${minutes} minutos son ${hours} horas.`;
+    document.getElementById("result1").textContent = `${minutes} minutos son ${hours} horas.`;
 }
 
 /*Ejercicio 2:  Calcular la normal estándar (z) dados los datos por el usuario: X=dato, ƒÊ=media, d=desviación. Formula (Z = X-M / d) */
@@ -11,13 +11,13 @@ function calculateZ() {
     const media = parseInt(document.getElementById("media").value);
     const desviacion = parseInt(document.getElementById("desviacion").value);
     const z = (dato - media) / desviacion;
-    document.getElementById("result").textContent = `Z = ${z}`;
+    document.getElementById("result2").textContent = `Z = ${z}`;
 }
 /*Ejercicio 3: Convertir °C a °F.*/
 function convertToFahrenheit() {
     const celsius = parseInt(document.getElementById("celsius").value);
     const fahrenheit = (celsius * 9 / 5) + 32;
-    document.getElementById("result").textContent = `${celsius} grados Celsius son ${fahrenheit} grados Fahrenheit.`;
+    document.getElementById("result3").textContent = `${celsius} grados Celsius son ${fahrenheit} grados Fahrenheit.`;
 }
 
 /*Ejercicio 4: Si una empresa tiene cierta cantidad de activos y cierta cantidad de pasivos? Cuál es su capital?. Formula (C = A-P)*/
@@ -25,14 +25,14 @@ function calculateCapital() {
     const activos = parseInt(document.getElementById("activos").value);
     const pasivos = parseInt(document.getElementById("pasivos").value);
     const capital = activos - pasivos;
-    document.getElementById("result").textContent = `El capital es de ${capital}.`;
+    document.getElementById("result4").textContent = `El capital es de ${capital}.`;
 }
 
 /*Ejercicio 5: Calcule la frecuencia de una onda que circula con un tiempo t. Formula (F=1/t)*/
 function calculateFrequency() {
     const tiempo = parseInt(document.getElementById("tiempo").value);
     const frequency = 1 / tiempo;
-    document.getElementById("result").textContent = `La frecuencia es de ${frequency}.`;
+    document.getElementById("result5").textContent = `La frecuencia es de ${frequency}.`;
 }
 
 /*Ejercicio 6:Calcule la potencia de un circuito con un voltaje V y una intensidad I. Formula (f = VI)*/
@@ -40,7 +40,7 @@ function calculatePower() {
     const voltaje = parseInt(document.getElementById("voltaje").value);
     const intensidad = parseInt(document.getElementById("intensidad").value);
     const power = voltaje * intensidad;
-    document.getElementById("result").textContent = `La potencia es de ${power} watts.`;
+    document.getElementById("result6").textContent = `La potencia es de ${power} watts.`;
 }
 
 /*Ejercio 7: Calcule el total que tendrá que pagar una persona que va al cine dependiendo del número de boletos a comprar y el precio.*/
@@ -48,7 +48,7 @@ function calculateTotal() {
     const boletos = parseInt(document.getElementById("boletos").value);
     const precio = parseInt(document.getElementById("precio").value);
     const total = boletos * precio;
-    document.getElementById("result").textContent = `El total a pagar es de $${total}.`;
+    document.getElementById("result7").textContent = `El total a pagar es de $${total}.`;
 }
 
 /*Ejercicio 8: Calcule las anualidades que tendrá que pagar una persona que pidió un préstamo. Dependiendo del tiempo que el elija y el interés 
@@ -58,7 +58,7 @@ function calculateAnualidad() {
     const tiempo = parseInt(document.getElementById("tiempo").value);
     const interes = parseInt(document.getElementById("interes").value);
     const anualidad = (prestamo / tiempo) * (1 + interes/100);
-    document.getElementById("result").textContent = `Las anualidades a pagar son de $${anualidad.toFixed(2)} por año.`;
+    document.getElementById("result8").textContent = `Las anualidades a pagar son de $${anualidad.toFixed(2)} por año.`;
   }
 
 /*Ejercicio 9: Determinar cuánto ganara una persona en base a las horas trabajadas. Tomando en cuenta el pago por hora. */
@@ -66,14 +66,14 @@ function calculateSalary() {
     const horas = parseInt(document.getElementById("horas").value);
     const pago = parseInt(document.getElementById("pago").value);
     const salario = horas * pago;
-    document.getElementById("result").textContent = `El salario es de $${salario} por ${horas} horas trabajadas.`;
+    document.getElementById("result9").textContent = `El salario es de $${salario} por ${horas} horas trabajadas.`;
 }
 
 /*Ejercicio 10: Convertir horas a segundos. */
 function convertToSeconds() {
   const horas = parseInt(document.getElementById("horas").value);
   const segundos = horas * 3600;
-  document.getElementById("result").textContent = `${horas} horas son ${segundos} segundos.`;
+  document.getElementById("result10").textContent = `${horas} horas son ${segundos} segundos.`;
 }
 
 /*Ejercicio 11: Calcular la fuerza. Formula (fuerza = trabajo / tiempo)*/
@@ -81,5 +81,5 @@ function calculateForce() {
     const trabajo = parseInt(document.getElementById("trabajo").value);
     const tiempo = parseInt(document.getElementById("tiempo").value);
     const fuerza = trabajo / tiempo;
-    document.getElementById("result").textContent = `La fuerza es de ${fuerza} Newtons.`;
+    document.getElementById("result11").textContent = `La fuerza es de ${fuerza} Newtons.`;
   }
